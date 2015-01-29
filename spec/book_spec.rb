ccfaf533e82a569e4e638c3e28d424e0abb5b55b
daynaclowe <daynalowe@Dayna-Lowes-MacBook-3.local>
2015-01-29 15:14:45 -0500

    Test to check for a title, author and category is provided, added attr_accessor for read/write of attributes

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -3,15 +3,30 @@ require 'spec_helper'
 describe Book do
 
 	before :each do
-		@book = Book.new "Title", "Author", :category
+		@book = Book.new "Title", "Author", "Category"
 	end
 
 	describe "#new" do
 		it "returns a new book object" do
 			@book.should be_an_instance_of Book
 		end
-		it "takes three parameters and returns a book object" do
+		it "throws an ArgumentError when given fewer than 3 parameters" do
 			lambda { Book.new "Title", "Author"}.should raise_exception ArgumentError
 		end
+	end
+
+	describe "#title" do
+		it "returns the correct title" do
+			@book.title.should eql "Title"
+		end
+
+		it "returns the correct author" do
+			@book.author.should eql "Author"
+		end
+
+		it "returns the correct category" do
+			@book.category.should eql "Category"
+		end		
+
 	end
 end
